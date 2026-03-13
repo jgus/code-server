7c35b97c3b18bd6f25b7e94f1e06288899c82f76
Josh Gustafson <j@gustafson.me>
2026-03-13 15:06:10 -0600

    Platforms

--- flake.nix
+++ flake.nix
@@ -11,41 +11,38 @@
     , nixpkgs
     , flake-utils
     , ...
-    }: flake-utils.lib.eachDefaultSystem (system:
+    }: flake-utils.lib.eachSystem
+    [ "x86_64-linux" "aarch64-linux" "armv7l-linux" "x86_64-darwin" "aarch64-darwin" ]
+    (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
 
-      # To update: change version, then set both hashes to "" and build.
+      # To update: change version, then set all hashes to "" and build.
       # The error messages will contain the correct hashes.
       # Release tags: https://github.com/coder/code-server/releases
       version = "4.111.0";
 
-      src =
-        let
-          sources = {
-            "x86_64-linux" = pkgs.fetchurl {
-              url = "https://github.com/coder/code-server/releases/download/v${version}/code-server-${version}-linux-amd64.tar.gz";
-              hash = "sha256-OPw+d3kdacQP5AD7m2gbto0yCn/vi09Re/6WtTzv8K0=";
-            };
-            "aarch64-linux" = pkgs.fetchurl {
-              url = "https://github.com/coder/code-server/releases/download/v${version}/code-server-${version}-linux-arm64.tar.gz";
-              hash = "sha256-BctYlG5qMIsSXTQOhzg2kgcnyS9jAGgVo45zWkQYz08=";
-            };
-          };
-        in
-          sources.${system} or (throw "Unsupported system: ${system}");
+      platformInfo = {
+        "x86_64-linux"   = { os = "linux"; arch = "amd64";  hash = "sha256-OPw+d3kdacQP5AD7m2gbto0yCn/vi09Re/6WtTzv8K0="; };
+        "aarch64-linux"  = { os = "linux"; arch = "arm64";  hash = "sha256-BctYlG5qMIsSXTQOhzg2kgcnyS9jAGgVo45zWkQYz08="; };
+        "armv7l-linux"   = { os = "linux"; arch = "armv7l"; hash = "sha256-dRCuHdypVDHQmjcirP0lOyn3M9LpIla2T7ufrOy2sl0="; };
+        "x86_64-darwin"  = { os = "macos"; arch = "amd64";  hash = "sha256-B/pQwLvzs16Ybka7oA+O97mhvahC0GL/3n7Vx+Veu8s="; };
+        "aarch64-darwin" = { os = "macos"; arch = "arm64";  hash = "sha256-EEclCKZHCQSk9cySEjjqQVcC3e4ogGHMonK3odhVcFA="; };
+      };
 
-      archSuffix = {
-        "x86_64-linux" = "amd64";
-        "aarch64-linux" = "arm64";
-      }.${system} or (throw "Unsupported system: ${system}");
+      info = platformInfo.${system};
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/coder/code-server/releases/download/v${version}/code-server-${version}-${info.os}-${info.arch}.tar.gz";
+        inherit (info) hash;
+      };
     in
     {
       packages.default = pkgs.stdenv.mkDerivation {
         pname = "code-server";
         inherit version src;
 
-        sourceRoot = "code-server-${version}-linux-${archSuffix}";
+        sourceRoot = "code-server-${version}-${info.os}-${info.arch}";
 
         nativeBuildInputs = [ pkgs.makeWrapper ];
 
@@ -68,7 +65,7 @@
           description = "VS Code in the browser";
           homepage = "https://github.com/coder/code-server";
           mainProgram = "code-server";
-          platforms = [ "x86_64-linux" "aarch64-linux" ];
+          platforms = builtins.attrNames platformInfo;
         };
       };
     });
